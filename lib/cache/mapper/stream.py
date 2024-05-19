dea97d82f7f254455fd26d816daf8949948e6534
algamanser <dondokov.gunga@bwg-io.site>
2024-05-19 20:03:42 +0300

    update stream cache expiration

--- lib/cache/mapper/stream.py
+++ lib/cache/mapper/stream.py
@@ -2,7 +2,7 @@ from lib.cache.mapper.base import BaseMapper
 
 class StreamMapper(BaseMapper):
     def create_streams(self, service, streams):
-        self.create(service, streams, expiration=100)
+        self.create(service, streams, expiration=21600)
     
     def get_streams(self, service):
         return self.get(service)
